c4102c262f9094452598f82f3665c14cab88440d
nguthuongtin <ngutin2014@gmail.com>
2026-09-21 22:22:11 +0700

    fix(api): add jwks-rsa and jose to serverExternalPackages to fix firebase-admin esm error on vercel

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  serverExternalPackages: ['firebase-admin'],
+  serverExternalPackages: ['firebase-admin', 'jwks-rsa', 'jose'],
   async headers() {
     return [
       {
